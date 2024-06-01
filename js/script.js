819e87d32feb489329b5c0fcd3f477fbec823f1f
cluster0 <fourmou.m@gmail.com>
2024-06-01 02:37:24 +0100

    GTM setup

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Dark mode toggle
+
+var toggle = document.getElementById("dark-mode-toggle");
+var darkTheme = document.getElementById("dark-mode-theme");
+var savedTheme = localStorage.getItem("dark-mode-storage") || "light";
+setTheme(savedTheme);
+
+toggle.addEventListener("click", () => {
+    if (toggle.className === "fa-solid fa-moon") {
+        setTheme("dark");
+    } else if (toggle.className === "fa-solid fa-sun") {
+        setTheme("light");
+    }
+});
+
+function setTheme(mode) {
+    localStorage.setItem("dark-mode-storage", mode);
+    if (mode === "dark") {
+        darkTheme.disabled = false;
+        toggle.className = "fa-solid fa-sun";
+    } else if (mode === "light") {
+        darkTheme.disabled = true;
+        toggle.className = "fa-solid fa-moon";
+    }
+}
+
+// Managing consent
+function getCookie(name) {
+    function escape(s) { return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); }
+    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
+    return match ? match[1] : null;
+}
+
+var consent = getCookie("consent-settings")
+
+if (consent.toString()[0] == 1) console.log("analytics ready to go")
+if (consent.toString()[1] == 1) console.log("funcitonal ready to go")
+if (consent.toString()[2] == 1) console.log("advertising ready to go")
